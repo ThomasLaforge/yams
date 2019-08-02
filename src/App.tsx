560200050e59a38770065a4ffc7206ef3fae4e1d
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-08-02 17:23:55 +0200

    transform dice, removing ref and passing rolling status prop

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,13 @@ import Dice from './components/Dice/Dice';
 import { Player } from './modules/Player';
 import { ScoringBoard as ScoringBoardModel } from './modules/ScoringBoard';
 import { Dice as DiceModel } from './modules/Dice';
-import { NB_DICES } from './modules/defs';
+import { NB_DICES, DiceValue } from './modules/defs';
 
 interface AppState {
   players: Player[],
   dices: DiceModel[],
-  nbRemainingRoll: number
+  nbRemainingRoll: number,
+  rolling: boolean
 }
 
 export default class App extends Component<{}, AppState> {
@@ -26,23 +27,33 @@ export default class App extends Component<{}, AppState> {
         new Player('Kevin', 'fddsskf', new ScoringBoardModel({}, {}))
       ],
       dices: Array(NB_DICES).fill('').map(e => new DiceModel()),
-      nbRemainingRoll: 1
+      nbRemainingRoll: 1,
+      rolling: false
     }
   }
 
   roll = () => {
     const dices = this.state.dices.slice()
-    dices.forEach(d => d.roll())
-    dices.forEach( (d,i) => i > 2 && d.lock())
-    this.setState({ dices, nbRemainingRoll: this.state.nbRemainingRoll - 1 })
+    dices.forEach( (d, i) => d.value = (i + 1) as DiceValue )
+    this.setState({ dices, nbRemainingRoll: this.state.nbRemainingRoll - 1, rolling: true }, () => {
+      setTimeout(() => {
+        this.setState({ rolling: false })
+      }, 750)
+    })
   }
 
   handleDiceClick = (d: DiceModel, index: number) => {
-    let index = -1
+    let nbOtherDiceWithSameLockStatus = 0
 
-    let dGlobalIndex = 0
+    let dGlobalIndex = this.state.dices.findIndex(dice => {
+      if(dice.isLocked === d.isLocked){
+        nbOtherDiceWithSameLockStatus++
+      }
+      return dice.isLocked === d.isLocked && nbOtherDiceWithSameLockStatus - 1 === index
+    })
+
     let dices = this.state.dices.slice()
-    dices[index].switchIsLocked()
+    dices[dGlobalIndex].switchIsLocked()
     this.setState({ dices })
   }
 
@@ -55,7 +66,11 @@ export default class App extends Component<{}, AppState> {
         
         <div className="dices-to-roll">
           {this.state.dices.filter(d => !d.isLocked).map( (d, k) => 
-            <Dice dice={d} onClick={this.handleDiceClick(d, k)} />
+            <Dice 
+              dice={d}
+              onClick={() => this.handleDiceClick(d, k)} 
+              roll={this.state.rolling}
+            />
           )}
         </div>
 
@@ -69,7 +84,7 @@ export default class App extends Component<{}, AppState> {
         <h2 className='dices-locked-title'>Locked dices</h2>
         <div className="dices-locked">
           {this.state.dices.filter(d => d.isLocked).map( (d, k) => 
-            <Dice dice={d} />
+            <Dice dice={d} onClick={() => this.handleDiceClick(d, k)} roll={false} />
           )}
         </div>
       </div>
--- src/components/Dice/DIce.tsx
+++ src/components/Dice/DIce.tsx
@@ -9,12 +9,13 @@ interface DiceProps {
     dice?: DiceModel,
     selected?: boolean,
     onClick?: Function
+    roll?: boolean
 }
 interface DiceState {
 
 }
 
-export default class Dice extends React.Component<DiceProps, DiceState> {
+export default class Dice extends React.PureComponent<DiceProps, DiceState> {
     private sides = [
         "translateZ(-100px) rotateY(0deg)",
         "translateZ(-100px) rotateX(-180deg)",
@@ -23,36 +24,28 @@ export default class Dice extends React.Component<DiceProps, DiceState> {
         "translateZ(-100px) rotateX(-90deg)",
         "translateZ(-100px) rotateX(90deg)"
     ];
-    private dieRef = React.createRef<HTMLDivElement>();
-
-    componentDidMount(){
-        let initRoll = !!this.props.dice ? this.props.dice.value - 1 : Math.floor(Math.random() * (this.sides.length))
-        this.dieRef.current!.style.transform = this.sides[initRoll];
-    }
-
-    componentDidUpdate(){
-        this.props.dice && !this.props.dice.isLocked && this.roll()
-    }
-
-    roll = () => {
-        let sides = this.sides
-        this.dieRef.current!.classList.add("rolling");
-
-        setTimeout( () => {
-            let roll = !!this.props.dice ? this.props.dice.value - 1 : Math.floor(Math.random() * (sides.length))
-            this.dieRef.current!.classList.remove("rolling");
-            this.dieRef.current!.style.transform = sides[roll];
-        }, 750);
-    }
 
     render(){
+
         let divs = this.sides.map((side, index) => {
             return <div key={index} className="side">{index+1}</div>
         });
+        let roll = !!this.props.dice ? this.props.dice.value - 1 : Math.floor(Math.random() * (this.sides.length))
 
         return (
-            <div className="die-container" onClick={() => this.props.onClick && this.props.onClick()} >
-                <div ref={this.dieRef} className={'die-content d' + this.sides.length}>
+            <div 
+                className="die-container" 
+                onClick={() => this.props.onClick && this.props.onClick()} 
+            >
+                <div 
+                    className={'die-content d' 
+                        + this.sides.length 
+                        + (!!this.props.roll ? ' rolling' : '')
+                    }
+                    style={{
+                        'transform': !this.props.roll ? this.sides[roll] : ''
+                    }}
+                >
                     { divs }
                 </div>
             </div>
